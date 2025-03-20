0acc336a3e0258037dc8ca3ab7a3e00a39d94a25
Adarsh053 <valleadarsh@gmail.com>
2025-03-20 20:04:56 +0530

    Showcasing my Skills using Portfolio

---
+++ postfolio.js
@@ -0,0 +1,14 @@
+let = new Typed(".text", {
+    strings: [" Mern Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+             
+             
+             
+             
+             
+             
+             
